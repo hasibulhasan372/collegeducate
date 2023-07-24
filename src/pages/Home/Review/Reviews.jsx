f0acf23f1787bd2c6689bc1a14eaca3387805877
HasibulHasan <hasibulhasan372@gmail.com>
2023-07-24 11:57:07 +0600

    create review section

---
+++ src/pages/Home/Review/Reviews.jsx
@@ -0,0 +1,13 @@
+import useReview from "../../../hooks/useReview";
+
+
+const Reviews = () => {
+    const [reviews] = useReview();
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Reviews;
